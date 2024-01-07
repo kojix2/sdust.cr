1232563f021a1dbff49ed7feaddcadcf42a662d6
kojix2 <2xijok@gmail.com>
2024-01-08 01:37:32 +0900

    Add experimental multi-threading mode for sdust

--- src/sdust/app.cr
+++ src/sdust/app.cr
@@ -24,13 +24,47 @@ module Sdust
     end
 
     def run
-      ReadFasta.each_contig(@in_file) do |name, sequence|
-        name = name.split.first
-        STDERR.puts "[sdust] #{name} #{sequence.size}bp"
-        result = Core.new.sdust(sequence, win_size, threshold)
-        result.each do |r|
-          puts "#{name}\t#{r >> 32}\t#{r.unsafe_as(Int32)}"
+      {% if flag?(:preview_mt) %}
+        STDERR.puts "[sdust] experimental multi-threading mode"
+        channel = Channel(Tuple(String, Array(UInt64))).new
+        names = [] of String
+
+        ReadFasta.each_contig(@in_file) do |long_name, sequence|
+          name = long_name.split.first
+          STDERR.puts "[sdust] #{name} #{sequence.size}bp"
+          names << name
+          spawn do
+            result = Core.new.sdust(sequence, win_size, threshold)
+            channel.send({name, result})
+          end
         end
+        results = {} of String => Array(UInt64)
+        while (!names.empty?)
+          name, result = channel.receive
+          if names.first == name
+            names.shift
+            print_result(name, result)
+            while results.has_key?(names.first)
+              n = names.shift
+              print_result(n, results.delete(n).not_nil!)
+            end
+          else
+            results[name] = result
+          end
+        end
+      {% else %}
+        ReadFasta.each_contig(@in_file) do |long_name, sequence|
+          name = long_name.split.first
+          STDERR.puts "[sdust] #{name} #{sequence.size}bp"
+          result = Core.new.sdust(sequence, win_size, threshold)
+          print_result(name, result)
+        end
+      {% end %}
+    end
+
+    def print_result(name, result, io = STDOUT)
+      result.each do |r|
+        io.puts "#{name}\t#{r >> 32}\t#{r.unsafe_as(Int32)}"
       end
     end
   end
