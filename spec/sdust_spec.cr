a02322b2e09461f06fcb21d370891abcbb1b87a9
kojix2 <2xijok@gmail.com>
2024-01-10 01:09:04 +0900

    Refactor Sdust spec

--- spec/sdust_spec.cr
+++ spec/sdust_spec.cr
@@ -1,35 +1,30 @@
 require "./spec_helper"
 
+def run_sdust(input_file : String, t : Int32, expected_output_file : String)
+  io = IO::Memory.new
+  Sdust::App.new(input_file, 20, t).run(io)
+  correct_result = File.read(expected_output_file)
+  io.to_s.gsub(/\r\n/, "\n").should eq correct_result
+end
+
 describe Sdust do
   it "has a version number" do
     Sdust::VERSION.should_not be_nil
   end
 
   it "run moo.fa with t=8" do
-    io = IO::Memory.new
-    Sdust::App.new("#{__DIR__}/fixtures/moo.fa", 20, 8).run(io)
-    correct_result_t08 = File.read("#{__DIR__}/fixtures/moo_t08.txt")
-    io.to_s.should eq correct_result_t08
+    run_sdust("#{__DIR__}/fixtures/moo.fa", 8, "#{__DIR__}/fixtures/moo_t08.txt")
   end
 
   it "run moo.fa with t=10" do
-    io = IO::Memory.new
-    Sdust::App.new("#{__DIR__}/fixtures/moo.fa", 20, 10).run(io)
-    correct_result_t08 = File.read("#{__DIR__}/fixtures/moo_t10.txt")
-    io.to_s.should eq correct_result_t08
+    run_sdust("#{__DIR__}/fixtures/moo.fa", 10, "#{__DIR__}/fixtures/moo_t10.txt")
   end
 
   it "run moo.fa.gz with t=8" do
-    io = IO::Memory.new
-    Sdust::App.new("#{__DIR__}/fixtures/moo.fa.gz", 20, 8).run(io)
-    correct_result_t08 = File.read("#{__DIR__}/fixtures/moo_t08.txt")
-    io.to_s.should eq correct_result_t08
+    run_sdust("#{__DIR__}/fixtures/moo.fa.gz", 8, "#{__DIR__}/fixtures/moo_t08.txt")
   end
 
   it "run moo.fa.gz with t=10" do
-    io = IO::Memory.new
-    Sdust::App.new("#{__DIR__}/fixtures/moo.fa.gz", 20, 10).run(io)
-    correct_result_t08 = File.read("#{__DIR__}/fixtures/moo_t10.txt")
-    io.to_s.should eq correct_result_t08
+    run_sdust("#{__DIR__}/fixtures/moo.fa.gz", 10, "#{__DIR__}/fixtures/moo_t10.txt")
   end
 end
